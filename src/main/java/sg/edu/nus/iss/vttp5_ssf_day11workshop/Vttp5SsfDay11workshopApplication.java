bc6a2992a9a648449e04d47734ef16d97e4458eb
dharmesh-b98 <dharmesh99123@hotmail.com>
2024-11-16 16:22:06 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/vttp5_ssf_day11workshop/Vttp5SsfDay11workshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp5_ssf_day11workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5SsfDay11workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5SsfDay11workshopApplication.class, args);
+	}
+
+}
